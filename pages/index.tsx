68ba3d1db52306faf9d89a216cb5e40568e1b340
JeremyMejia <jeremy9451.lmd@gmail.com>
2023-06-05 00:00:42 -0500

    Change index for build

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0' >
       <Head>
-        <title >Jeremy's Portfolio</title>
+        <title >Jeremys Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
