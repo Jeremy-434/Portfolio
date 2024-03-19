04b7219ca68aa14a40c427c407b7e0f3314da09d
Jeremy Mejia <jeremy9451.lmd@gmail.com>
2024-03-19 12:48:17 -0500

    console.log for deploy test added

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -4,6 +4,7 @@ export const fetchPageInfo = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`, { method: 'GET' })
 
   console.log('🚀 ~ res.status:', res.status)
+  console.log('🚀 ~ NEXT_PUBLIC_BASE_URL:', process.env.NEXT_PUBLIC_BASE_URL)
   if (!res.ok) {
     throw new Error(`HTTP error! status: ${res.status}`)
   } else {
