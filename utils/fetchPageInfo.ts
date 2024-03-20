980ffca8612a2f81f2149979b29fbc4b7fe9dbef
Jeremy Mejia <jeremy9451.lmd@gmail.com>
2024-03-20 14:48:38 -0500

    only test, dont worry

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -4,10 +4,12 @@ export const fetchPageInfo = async () => {
   const res = await fetch(`https://${process.env.VERCEL_URL}/api/getPageInfo`, { method: 'GET' })
 
   console.log('🚀 ~ res.status:', res.status)
+  console.log('🚀 ~ res.ok:', res.ok)
   console.log('🚀 ~ res:', res)
   console.log('🚀 ~ NEXT_PUBLIC_BASE_URL:', process.env.NEXT_PUBLIC_BASE_URL)
   console.log('🚀 ~ VERCEL_URL:', process.env.VERCEL_URL)
   if (!res.ok) {
+    console.log('🚀 ~ res.ok:', res.ok)
     console.log('🚀 ~ res.status:', res.status)
     throw new Error(`HTTP error! status: ${res.status}`)
   } else {
