8c243360170a790d8653c032c05de0bbd881757e
Jeremy Mejia <jeremy9451.lmd@gmail.com>
2024-03-20 13:50:16 -0500

    new console.logs in utils/fetchPageInfo.ts for debug in vercel

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -4,13 +4,16 @@ export const fetchPageInfo = async () => {
   const res = await fetch(`https://${process.env.VERCEL_URL}/api/getPageInfo`, { method: 'GET' })
 
   console.log('🚀 ~ res.status:', res.status)
+  console.log('🚀 ~ res:', res)
   console.log('🚀 ~ NEXT_PUBLIC_BASE_URL:', process.env.NEXT_PUBLIC_BASE_URL)
   console.log('🚀 ~ VERCEL_URL:', process.env.VERCEL_URL)
   if (!res.ok) {
     throw new Error(`HTTP error! status: ${res.status}`)
   } else {
     const data = await res.json()
+    console.log('🚀 ~ data:', data)
     const pageInfo: PageInfo = data.pageInfo
+    console.log('🚀 ~ pageInfo:', pageInfo)
 
     return pageInfo
   }
